665d23eefe1cf9f113dc9a8a5a7ec1fe2b68a066
Simon Skinner <codewith@vultuk.me>
2015-01-08 00:17:52 +0000

    Changed 'to' method to use property style. This will allow sub categories of Services in the future

--- spec/Vultuk/Connections/ConnectSpec.php
+++ spec/Vultuk/Connections/ConnectSpec.php
@@ -14,12 +14,12 @@ class ConnectSpec extends ObjectBehavior
 
     function it_should_return_a_service()
     {
-        $this->to('Testing', 'PostTestServerCom')->shouldHaveType('\Vultuk\Connections\Service\Testing\PostTestServerCom');
+        $this->to('Testing.PostTestServerCom')->shouldHaveType('\Vultuk\Connections\Service\Testing\PostTestServerCom');
     }
 
     function it_should_post_data_to_test_provider()
     {
-        $this->to('Testing', 'PostTestServerCom')
+        $this->to('Testing.PostTestServerCom')
              ->setData(['Super'=>'Test', 'Woop' => 'Working'])
              ->send()
              ->shouldHaveType('Vultuk\Connections\Result');
--- src/Vultuk/Connections/Connect.php
+++ src/Vultuk/Connections/Connect.php
@@ -6,10 +6,10 @@
     {
         protected $provider;
 
-        public static function to($serviceType, $provider)
+        public static function to($provider)
         {
             $thisClass = new Static();
-            $thisClass->provider = '\\Vultuk\\Connections\\Service\\'.$serviceType.'\\'.$provider;
+            $thisClass->provider = '\\Vultuk\\Connections\\Service\\'.str_replace('.', '\\', $provider);
             return new $thisClass->provider();
         }
     }
